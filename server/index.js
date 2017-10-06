2ebaa722600d7aa9fa6c25f22432f252f5c35bbc
sarahheacock <seheacock@bellsouth.net>
2017-10-06 10:23:25 -0400

    moved server

--- server/index.js
+++ server/index.js
@@ -91,12 +91,12 @@ app.use(forceSSL());
 // ==================STATIC REQUESTS====================
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '../dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '../dist/index.html'));
 });
 
 //===========================================================
